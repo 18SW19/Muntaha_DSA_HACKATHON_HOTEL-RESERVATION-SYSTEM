545c7223a72dc4f8927f1cbe6ef6289745d7e5c5
18SW19 <49391905+18SW19@users.noreply.github.com>
2019-11-29 00:25:03 +0500

    18SW02, 18SW14, 18SW110

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.LinkedList;
+// import java.util.io;
 class HotelReservation 
 {
       
@@ -9,67 +9,141 @@ class HotelReservation
 	     
 	     	 Scanner scan = new Scanner (System.in);
 	          int search;
-	     System.out.println("\n\n****************************************\n");
+	          int room;
+	          int [] arr= new int[5];
+	          PriorityQueue q=new PriorityQueue();
+	     System.out.println("\n\n\t\t****************************************\n");
+	     System.out.println("\t\t\tHOTEL RESERVATION SYSTEM");    
+	     System.out.println("\n\n\t\t****************************************\n");
 	     System.out.println("\t\t1 : Booking Room ");
 	     System.out.println("\t\t2 : Food Corner ");
 	     System.out.println("\t\t3 : Exit");
-	      System.out.println("\n\n****************************************\n");
-	     int input=scan.nextInt();
+	     System.out.println("\n\n\t\t****************************************\n");
+
+
+          System.out.println("Enter your choice");
+	      search=scan.nextInt();
+
 	      switch (search)
 	      {
 	      	case 1:
 	      	{
-	      		Room(); 
-	      		break;
-	      	}
+                System.out.println("Rooms Number");
+	            arr  = new int [] { 1 , 2 , 3 , 4 ,5,6,7,8 , 9 , 10} ;
+	            
+
+	             for(int i=0;i<arr.length;i++)
+	             {
+	                   System.out.println(arr[i]+"\t");
+	             }
+	             System.out.println("Enter your choice");
+	             room = scan.nextInt();
+                     if(room==1)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 1");
+                     }
+
+                     if(room==2)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 2");
+                     }
+                     if(room==3)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 3");
+                     }
+                     if(room==4)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 4");
+                     }
+                     if(room==5)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 5");
+                     }
+                     if(room==6)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 6");
+                     }
+                     if(room==7)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 7");
+                     }
+                     if(room==8)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 8");
+                     }
+                     if(room==9)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 9");
+                     }if(room==10)
+                     {
+                     	System.out.println("Congractulation your room is booked");
+                     	System.out.println("Your room no is 10");
+                     }  
+                         
+                       else System.out.println("Room is not available");
+                     
+
+                     break;
+    	
+             }
+    
+	      		
+	      		
+	      	
 	      	case 2:
 	      	{
-	      		Food();
-                break;
+	      			
+ 	
+
+				 	System.out.println("**********MENU***************");
+				 	System.out.println("1 :Burger     180");
+				 	System.out.println("2 :Pasta      150");
+				 	System.out.println("3 :Pizza      250");
+				 	System.out.println("4 :Biryani    240");
+				 	System.out.println("Enter your choice");
+				 	int choice=scan.nextInt();
+
+                     
+
+				  
+				    break;
+	      	}
+	      	case 3:
+	      	{
+	      		System.exit(0);
+	      	}
+	      	default :
+	      	{
+	      		System.out.println("Invalid Number");
+
 	      	}
       
          }
      }
 	
 }
-class Room
-{
-	//LinkedList <String> = new Linkedlist<>();
-    
-	int Array [] = {1,2,3,4,5,6,7,8,9,10};
-	System.out.println("Rooms Number");
-	for(int i=0;i<Array.length;i++)
-	{
-	    System.out.println(Array[i]);
-    }
-	public void findRoom()
-    {
-    	int room;
-    	Scanner in= new Scanner(System.in);
-    	for ( room=0;room<Array[i].length ;room++) {
-    		System.out.println("Choose your room");
-    		Array[room]=in.nextInt();
 
-    	}
+
+
    
-    }
-}	
+	
       
- public class Food
- {
- 	Scanner f= new Scanner (System.in);
- 	LinkedList<String> list = new LinkedList<>();
- 	
-
- 	list.add("MENU");
- 	list.add("Burger");
- 	list.add("PAsta");
- 	list.add("Pizza");
- 	list.add("Biryani");
-    System.out.println(list);
+ 
+ 
+ 
+ 
     
      
 
- } 
+
 
 
